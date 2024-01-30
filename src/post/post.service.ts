0272aaad595bb1cd4073ccdb4cf5efe3f370553f
Gabriel Lomenha <gabriellomenha@gmail.com>
2024-01-30 19:06:58 -0300

    construtores de cada entidade com importações

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PostService {
+  constructor(
+    private prisma: PrismaService
+  ) {}
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
