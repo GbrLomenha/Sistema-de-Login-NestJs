fb72c4599b55f89713ebc174e4fe800cfa4695da
Gabriel Lomenha <gabriellomenha@gmail.com>
2024-01-31 15:57:25 -0300

    criação da dto com class-validator e ativação do class-validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator'
+export class CreateUserDto {
+    
+    @IsNotEmpty()
+    @IsEmail()
+    email:String
+    
+    @IsNotEmpty()
+    @IsString()
+    name:String
+    
+    @IsNotEmpty()
+    @IsString()
+    password:String
+}
