69cd20a05658cd8d3e05dda423482e4dfa6681de
Gabriel Lomenha <gabriellomenha@gmail.com>
2024-01-31 14:35:26 -0300

    Adição da estratégia no modulo e criação da função de validação de usuario

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService,PrismaService],
+    providers: [AuthService,PrismaService,LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor( private readonly prisma : PrismaService){}
+    constructor(
+        private readonly prisma : PrismaService,
+        private readonly userService: UserService,    
+    ){}
 
+    async validateUser(email,senha){
+        const user = await this.userService.findByEmail(email)
+        
+        if(!user || user.password != senha){
+            throw new Error('Credenciais inválidas')
+        }
+        return true
+    }
 }
