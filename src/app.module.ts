e3107588ddf7dee0e5249e542b95e0273b0af8b3
Gabriel Lomenha <gabriellomenha@gmail.com>
2024-01-31 01:37:37 -0300

    adição do diretório auth e configuração inicial do AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [UserModule, PostModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, PrismaService, AuthService],
 })
 export class AppModule {}
